a65510112cc62f10ae19dec4368569888202d219
Abhinav Jha <Abhi0803@github.com>
2020-12-12 10:58:21 +0530

    Updates

---
+++ precache-manifest.f2d821ce325678311f2c5cf13c5807f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc7d38f3f652542f4dbd6e1ed326db2",
+    "url": "/keeper-app-React/index.html"
+  },
+  {
+    "revision": "771887f48e7ade571689",
+    "url": "/keeper-app-React/static/js/2.8f3899f3.chunk.js"
+  },
+  {
+    "revision": "f239ca090d21aa155e25",
+    "url": "/keeper-app-React/static/js/main.95ad5812.chunk.js"
+  },
+  {
+    "revision": "668f12f06ffc2d723b01",
+    "url": "/keeper-app-React/static/js/runtime-main.dfe08ee8.js"
+  }
+]);
